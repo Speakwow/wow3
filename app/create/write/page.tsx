4620632f24b7fc24d200d4b34ff775ddf838d169
Noxr3 <729593051@qq.com>
2024-08-05 18:00:12 +0800

    fix: write backward button

--- app/create/write/page.tsx
+++ app/create/write/page.tsx
@@ -25,7 +25,7 @@ export default async function WriteCreator() {
         <div className="relative w-full h-screen bg-muted">
             <ScrollArea className="h-full overflow-hidden">
                 <div className="absolute top-2 left-2">
-                    <Link href={"./"} className="z-10">
+                    <Link href={"/"} className="z-10">
                         <Button size="icon" variant="outline">
                             <ArrowLeftIcon />
                         </Button>
