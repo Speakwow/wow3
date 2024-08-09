044d130f9818fc5088bfdb0504877ad208430870
Noxr3 <729593051@qq.com>
2024-08-09 12:01:50 +0800

    fix: dialog & popover

--- app/dashboard/assignment/action.tsx
+++ app/dashboard/assignment/action.tsx
@@ -180,7 +180,7 @@ export function NewAssignmentButton(
                           </Button>
                         </FormControl>
                       </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
+                      <PopoverContent className="w-auto p-0 z-20" align="start">
                         <Calendar
                           initialFocus
                           mode="range"
