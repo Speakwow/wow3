3ffb39dd9b779f92063d88f52b969816e5f42f69
Noxr3 <729593051@qq.com>
2024-08-26 22:10:41 +0800

    fix:student see endAt

--- components/assignment-card.tsx
+++ components/assignment-card.tsx
@@ -19,14 +19,15 @@ import { Badge } from "@/components/ui/badge";
 import { useState } from "react";
 import { useRouter } from "next/navigation"
 import { Separator } from "./ui/separator";
-import { Score2Grade } from "@/lib/tools";
+import { getBeijingTime, hoursUntil, Score2Grade } from "@/lib/tools";
+import { isAfter, isBefore } from "date-fns";
 
 
 
 
 
-export function AssignCard({ userId, userData, name, type, id, intro, cover, tag, record }:
-    { userId: string, userData: any, name: string, type: string, id: string, intro: string, cover: string, tag: string, record: any }) {
+export function AssignCard({ userId, startAt, endAt, name, type, id, intro, cover, tag, record }:
+    { userId: string, startAt: Date, endAt: Date, name: string, type: string, id: string, intro: string, cover: string, tag: string, record: any }) {
     let coverImg = ''
     if (!cover) {
         coverImg = 'https://imagedelivery.net/yeOpFSfmW-7M72sPdtpMKw/108cf320-27a7-4110-0312-6f0b32223200/avatar'
@@ -36,7 +37,30 @@ export function AssignCard({ userId, userData, name, type, id, intro, cover, tag
 
 
     const router = useRouter()
+    let hoursleft = hoursUntil(endAt)
+    if (hoursleft < 0) {
+        hoursleft = 0
+    } else {
+        hoursleft = +hoursleft.toFixed(0)
+    }
 
+    function toBeijingTime(date: Date): string {
+        return date.toLocaleString('zh-CN', {
+            timeZone: 'Asia/Shanghai',
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false, // 使用 24 小时制
+        }).replace(/\//g, '-'); // 将日期中的 '/' 替换为 '-'
+    }
+
+    const today = getBeijingTime()
+    let active = true
+    if(isAfter(today, endAt)||isBefore(today,startAt)){
+        active = false
+    }
 
     return (
         <div className="relative w-full h-fit">
@@ -56,8 +80,8 @@ export function AssignCard({ userId, userData, name, type, id, intro, cover, tag
                     }
                 </DropdownMenuContent>
             </DropdownMenu> */}
-            <Link href={`/${type}/${id}`}>
-                <Card className={`relative flex flex-col bg-white rounded-[10px] border hover:ring focus:outline-none focus:ring focus:ring-[#42C83C] p-2 gap-2 hover:ring-[#42C83C]`}>
+            <Link href={active?`/${type}/${id}`:'#'}>
+                <Card className={`relative flex flex-col bg-white rounded-[10px] border hover:ring focus:outline-none focus:ring p-2 gap-2  ${active?"focus:ring-[#42C83C] hover:ring-[#42C83C]":"hover:ring-gray-200 focus:ring-gray-200"}`}>
                     <div className="flex flex-row gap-3">
                         <div className="h-full w-[100px]">
                             <Image
@@ -83,8 +107,9 @@ export function AssignCard({ userId, userData, name, type, id, intro, cover, tag
 
                         </div>
                     </div>
-                    <div className="flex flex-col w-full">
-                        <Separator />
+                    <Separator />
+                    <div className="flex flex-col w-full ">
+                        
                         <div className="flex flex-row items-center justify-between p-2">
                             <div className="text-sm text-muted-foreground">
                                 成绩
@@ -99,6 +124,17 @@ export function AssignCard({ userId, userData, name, type, id, intro, cover, tag
                                         未完成
                                     </Badge>
                             }
+                        </div>
+                        <div className="flex flex-row items-center justify-between p-2">
+                            <div className="text-sm text-muted-foreground">
+                                截止时间
+                            </div>
+
+
+                            <Badge className="text-sm px-4 font-bold" variant="secondary">
+                                {toBeijingTime(endAt)}
+                            </Badge>
+
                         </div>
                     </div>
                 </Card>
